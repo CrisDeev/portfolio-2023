0c87349aa0531d5c28fad9f52c7392f88e71653f
Cristian <alvarezmartinez132@gmail.com>
2023-06-29 00:41:13 -0500

    Fix(Conocimiento.tsx)

--- src/components/sections/Conocimientos.tsx
+++ src/components/sections/Conocimientos.tsx
@@ -2,7 +2,7 @@ import { DataSkills } from '../../const'
 
 export const Conocimientos = (): JSX.Element => {
   return (
-    <section className="h-screen py-32 px-14 lg:py-36 xl:py-44 2xl:py-48" id='Conocimientos'>
+    <section className="h-auto py-32 px-14 lg:py-36 xl:py-44 2xl:py-48" id='Conocimientos'>
       <div className='flex flex-col items-center gap-5 '>
         <h1 className='h-32 text-4xl font-bold text-transparent bg-clip-text mobile:text-4xl bg-gradient-to-tr from-purple-300 via-purple-800 to-cyan-400 md:text-6xl'>Conocimientos</h1>
         <div className="h-full w-full flex flex-wrap justify-center gap-6 mobile:gap-14 xl:w-[85%] xl:gap-20">
