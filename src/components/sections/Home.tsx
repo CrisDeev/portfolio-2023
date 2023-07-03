965b20cf1c1a1da099d50238493631c0a604093c
Cristian Álvarez <94159947+CristianAlvarezM@users.noreply.github.com>
2023-07-02 20:44:02 -0500

    fix(curriculum ulr)

--- src/components/sections/Home.tsx
+++ src/components/sections/Home.tsx
@@ -28,7 +28,7 @@ export const Home = (): JSX.Element => {
         }}
         />
         <a
-          href='.https://firebasestorage.googleapis.com/v0/b/portfolio-aabd1.appspot.com/o/Cristian%20Alvarez.pdf?alt=media&token=971c8d14-b32b-4c5e-8046-d3bc71974a9f'
+          href='https://firebasestorage.googleapis.com/v0/b/portfolio-aabd1.appspot.com/o/Cristian%20Alvarez.pdf?alt=media&token=2b2dee2c-6d23-437d-90fb-50f1d3cc2783'
           rel='noopener noreferrer'
           target='_blank'
           className='border-[#3694ff] border-2 w-32 py-3 rounded-md text-[#3694ff] flex justify-center items-center gap-1 hover:text-purple-800 +-
