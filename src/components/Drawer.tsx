5d80d960cfa7645e913ef4ec66c455976a5518f0
Cristian Álvarez <94159947+CristianAlvarezM@users.noreply.github.com>
2023-07-19 14:53:53 -0500

    fix bug Drawer.tsx

--- src/components/Drawer.tsx
+++ src/components/Drawer.tsx
@@ -5,18 +5,23 @@ interface Props {
 
 export const Drawer: React.FC<Props> = ({ menuButton, handleMenuButton }) => {
   return (
-    <div className={`fixed z-[100] w-[300px] h-screen bg-[#272828] ${menuButton ? 'right-0' : '-right-full'} top-14 flex flex-col items-start text-2xl p-8 gap-10 transition-all duration-500 text-white md:hidden`}>
+    <div className={`fixed z-[100] w-[300px] h-screen bg-[#272828] ${menuButton ? 'right-0' : '-right-full'} top-14 flex flex-col items-start text-2xl p-8 gap-10 transition-all duration-500 md:hidden`}>
         <a
         onClick={handleMenuButton}
-        href="#About">Sobre mi</a>
+        className="text-white"
+        href="#About">Sobre mi
+        </a>
         <a
         onClick={handleMenuButton}
+        className="text-white"
         href="#Projects">Proyectos</a>
         <a
         onClick={handleMenuButton}
+        className="text-white"
         href="#Conocimientos">Habilidades</a>
         <a
         onClick={handleMenuButton}
+        className="text-white"
         href="#Contacto">Contactame</a>
     </div>
   )
