932ee37a36cff88d563b9d5d2f1fe5012dfebe1d
CristianAlvarezM <alvarezmartinez132@gmail.com>
2024-05-01 22:13:07 -0400

    update

--- src/components/sections/About.tsx
+++ src/components/sections/About.tsx
@@ -8,7 +8,7 @@ export const About = (): JSX.Element => {
           <p className="text-white text-center text-sm md:text-lg mobile:w-[70%] md:w-[50%] xl:text-2xl 2xl:text-4xl">
             👨‍💻 21 años. Soy de <span className="bg-clip-text text-transparent bg-gradient-to-tr from-yellow-300 via-blue-900 to-rose-700 font-bold">Colombia.</span> Desarrollador web con muchas ganas de crecer como profesional, creando soluciones innovadoras para proyectos web. Tengo una buena comunicación para trabajar en equipo. Además, estoy abierto a aprender cualquier tecnología que sea necesaria para poder aportar valor a cualquier empresa.
           </p>
-          <img className="rounded-[40px] w-4/5 mobile:w-[60%] md:w-[40%] lg:w-[40%] 2xl:w-[35%]" src='https://firebasestorage.googleapis.com/v0/b/portfolio-aabd1.appspot.com/o/Portfolio-Images%2Fme.jpeg?alt=media&token=f75c45ec-07ed-4111-895a-3441ad97bead' alt="Imagen personal"/>
+          <img className="rounded-[40px] w-4/5 mobile:w-[60%] md:w-[40%] lg:w-[40%] 2xl:w-[35%]" src='https://firebasestorage.googleapis.com/v0/b/portfolio-aabd1.appspot.com/o/Portfolio-Images%2Fme.jpeg?alt=media&token=f75c45ec-07ed-4111-895a-3441ad97bead' alt="Imagen personal" />
         </div>
       </div>
     </section>
